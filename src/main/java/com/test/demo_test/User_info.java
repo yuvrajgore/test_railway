9908d70f42b13655273d3d00d55bd027898481b2
yuvra <yuvrajgore123@gmail.com>
2025-08-31 13:02:11 +0530

    getter and setter added

--- src/main/java/com/test/demo_test/User_info.java
+++ src/main/java/com/test/demo_test/User_info.java
@@ -12,4 +12,20 @@ public class User_info {
     private UUID id;
 
     private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
